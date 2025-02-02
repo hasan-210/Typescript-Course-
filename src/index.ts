4fa081634be4e58d2bfd10735014b93e448db076
hasan-1 <hasanjasser551@gmail.com>
2025-02-02 16:53:38 +0200

    array | tuple | object | void

--- src/index.ts
+++ src/index.ts
@@ -13,3 +13,11 @@ const userProfile:{
     isMarried:false,
     hobbies:["football","Cooking"]
 }
+
+
+const list:[number,string,{age:number},boolean] = [ 1 , "hasan", {age:25} , false]
+
+// function logMessage(message: string): void
+function logMessage(message:string){
+    console.log(message);
+}
