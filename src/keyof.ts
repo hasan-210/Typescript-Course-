75c35ee8beb45f68014e5d4a150917cd15d89251
hasan-1 <hasanjasser551@gmail.com>
2025-02-06 10:32:50 +0200

    Funtion With Array Destructuring

--- src/keyof.ts
+++ src/keyof.ts
@@ -17,6 +17,16 @@ type AnimalKeys = keyof IAnimal ; /** key : animalName | age | color */
 function showAnimal(obj:IAnimal,key:AnimalKeys){
     return obj[key] ;
 }
-console.log('Animal Name : ' + showAnimal(animal,"animaleName"));
-console.log('Animal Color : '+showAnimal(animal,"color"));
-console.log('Animal Age : '+showAnimal(animal,"age"));
+// console.log('Animal Name : ' + showAnimal(animal,"animaleName"));
+// console.log('Animal Color : '+showAnimal(animal,"color"));
+// console.log('Animal Age : '+showAnimal(animal,"age"));
+
+
+/** Funtion With Array Destructuring */
+
+
+function printLanguage([a,b]:string[]){
+    return `FirstLang : ${a} , SecondLang : ${b}`;
+}
+
+console.log(printLanguage(['javascript','php','python']));
