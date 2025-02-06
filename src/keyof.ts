a75d07f5d787dbc0aec83d2fde7fba7dcaadf582
hasan-1 <hasanjasser551@gmail.com>
2025-02-06 10:20:06 +0200

    How To Use Keyof | interface | type

---
+++ src/keyof.ts
@@ -0,0 +1,22 @@
+
+
+interface IAnimal {
+    animaleName:string,
+    color:string,
+    age:number
+}
+
+const animal:IAnimal = {
+    animaleName:"bird",
+    color:"green",
+    age:30
+}
+
+type AnimalKeys = keyof IAnimal ; /** key : animalName | age | color */
+
+function showAnimal(obj:IAnimal,key:AnimalKeys){
+    return obj[key] ;
+}
+console.log('Animal Name : ' + showAnimal(animal,"animaleName"));
+console.log('Animal Color : '+showAnimal(animal,"color"));
+console.log('Animal Age : '+showAnimal(animal,"age"));
