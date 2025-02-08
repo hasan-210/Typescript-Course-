20d57bc35f8862765e35fe8c2195773070125f04
hasan-1 <hasanjasser551@gmail.com>
2025-02-08 19:21:42 +0200

    index signature

---
+++ src/indexSignature.ts
@@ -0,0 +1,13 @@
+
+
+interface ICityDictionary {
+    [key:string] : string
+}
+
+const cityDictionary: ICityDictionary = {
+    damascuse : "Syria",
+    cairo     : "Egypt",
+
+}
+
+console.log(cityDictionary.cairo);
